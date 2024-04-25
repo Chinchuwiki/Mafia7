1ccad4d046ee3fcfbd1b24672b67283714176485
GastlyVirus <gstnedgrdrch93@gmail.com>
2024-04-25 16:56:10 -0300

    Integration of bootstrap to the project

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,7 +9,13 @@ function Navbar() {
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">
                         <img src={logoM7} alt="Bootstrap" width="70" height="50" />
+                        MAFIA 7
                     </a>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li>
+                    </ul>
                 </div>
             </nav>
         </>
